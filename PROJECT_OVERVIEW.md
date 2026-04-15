49d0a086bb90a28f377d83f73114a9ad6af72350
Jerry Zhao <ch3nzhao@gmail.com>
2026-04-16 00:02:12 +0800

    Rename server to keylane and update celer

--- PROJECT_OVERVIEW.md
+++ PROJECT_OVERVIEW.md
@@ -18,7 +18,7 @@ limitations under the License.
 
 ## Goal
 
-`celer_redis` is a Redis/Valkey-protocol server built on top of the `celer` core runtime.
+`keylane` is a Redis/Valkey-protocol server built on top of the `celer` core runtime.
 
 This repository should own:
 
---
+++ app/keylane.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2026 EloqData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstdint>
+#include <cstdlib>
+#include <string>
+#include <string_view>
+
+#include "celer/base/log.h"
+#include "celer/redis/server.h"
+
+int main(int argc, char** argv) {
+  std::string_view bind_ip = "127.0.0.1";
+  std::uint16_t port = 6379;
+  unsigned thread_count = 1;
+  int idle_timeout_ms = -1;
+
+  if (argc >= 2) {
+    bind_ip = argv[1];
+  }
+  if (argc >= 3) {
+    port = static_cast<std::uint16_t>(std::stoi(argv[2]));
+  }
+  if (argc >= 4) {
+    thread_count = static_cast<unsigned>(std::stoul(argv[3]));
+    if (thread_count == 0) {
+      CELER_LOG_ERROR << "thread_count must be >= 1";
+      return 1;
+    }
+  }
+  if (argc >= 5) {
+    idle_timeout_ms = std::stoi(argv[4]);
+  }
+
+  return celer::redis::RunServer(bind_ip, port, thread_count, idle_timeout_ms);
+}
