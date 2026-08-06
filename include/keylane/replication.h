9c0501be9a66e72478b543c9e619911d2550275e
Jerry Zhao <ch3nzhao@gmail.com>
2026-08-06 08:10:19 +0000

    Add partition replication and database epochs

---
+++ include/keylane/replication.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2026 EloqData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <atomic>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "celer/rpc/rpc.h"
+#include "celer/runtime/task.h"
+
+namespace celer {
+class Service;
+class Worker;
+}  // namespace celer
+
+namespace keylane::storage {
+class StorageEngine;
+}  // namespace keylane::storage
+
+namespace keylane {
+
+struct ReplicationOptions {
+  std::uint16_t listen_port = 0;
+  std::string target_ip;
+  std::uint16_t target_port = 0;
+  bool replica_read_only = false;
+};
+
+class ReplicationManager {
+ public:
+  ReplicationManager(storage::StorageEngine* storage,
+                     ReplicationOptions options);
+  ReplicationManager(const ReplicationManager&) = delete;
+  ReplicationManager& operator=(const ReplicationManager&) = delete;
+  ~ReplicationManager();
+
+  celer::Service* service() noexcept;
+  void StorageReady(celer::Worker& worker);
+  bool replica_read_only() const noexcept {
+    return options_.replica_read_only;
+  }
+
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+  ReplicationOptions options_;
+};
+
+}  // namespace keylane
--- include/keylane/server.h
+++ include/keylane/server.h
@@ -22,6 +22,8 @@
 #include <string_view>
 #include <vector>
 
+#include "keylane/replication.h"
+
 namespace keylane {
 
 int RunServer(std::string_view bind_ip, std::uint16_t port, unsigned thread_count,
@@ -31,6 +33,7 @@ int RunServer(std::string_view bind_ip, std::uint16_t port, unsigned thread_coun
               std::uint32_t flush_max_ms, std::size_t flush_size_bytes,
               bool verify_read_crc,
               const std::vector<std::string>& data_files,
-              std::uint64_t data_file_size_bytes);
+              std::uint64_t data_file_size_bytes,
+              ReplicationOptions replication_options = {});
 
 }  // namespace keylane
