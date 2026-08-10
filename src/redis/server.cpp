b2a70bb8c1d3c98944f3c684f5981664b9acd4bf
Jerry Zhao <ch3nzhao@gmail.com>
2026-08-10 06:08:16 +0000

    perf: make mimalloc mandatory

--- src/redis/server.cpp
+++ src/redis/server.cpp
@@ -16,6 +16,7 @@
 
 #include "keylane/server.h"
 
+#include <mimalloc.h>
 #include <poll.h>
 #include <sys/eventfd.h>
 #include <sys/socket.h>
@@ -648,6 +649,10 @@ Task<absl::Status> RedisService::Serve(TcpStream& stream,
 }  // namespace
 
 int RunServer(ServerOptions options) {
+  spdlog::info("mimalloc purge_delay={} arena_eager_commit={} allow_thp={}",
+               mi_option_get(mi_option_purge_delay),
+               mi_option_get(mi_option_arena_eager_commit),
+               mi_option_get(mi_option_allow_thp));
   spdlog::info(
       "keylane listening on {}:{} metrics_port={} threads={} "
       "idle_timeout_ms={} "
