ba912873ba7125d3298defdde5309ca9aa70a9da
Jerry Zhao <ch3nzhao@gmail.com>
2026-08-10 04:42:36 +0000

    feat: support disk-backed large keys

--- include/keylane/server.h
+++ include/keylane/server.h
@@ -22,6 +22,7 @@
 #include <vector>
 
 #include "keylane/replication.h"
+#include "keylane/storage/format.h"
 
 namespace keylane {
 
@@ -35,6 +36,7 @@ struct ServerOptions {
   unsigned busy_poll_us_ = 0;
   std::size_t registered_buffer_bytes_ = 16ULL * 1024 * 1024;
   std::uint64_t max_memory_bytes_ = 0;
+  std::size_t inline_key_max_bytes_ = storage::kDefaultInlineKeyBytes;
   std::uint32_t flush_max_ms_ = 1000;
   std::size_t flush_size_bytes_ = 8192ULL * 1024;
   bool verify_read_crc_ = true;
